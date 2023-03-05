ffdbf07a7a3836f4b8ce7070d05c8a405d7616f0
Jiang <jiangyue0380@gmail.com>
2023-03-05 22:12:00 +1100

    updated custom.js with examples of parameters and arguments of a function

--- Module-2-Functions/Functions-custom/custom.js
+++ Module-2-Functions/Functions-custom/custom.js
@@ -17,3 +17,19 @@ calculateBill()
 // capture the return value of the function
 const myTotal = calculateBill(); 
 console.log(`total bill is $${myTotal}`)
+
+// passing a param/parameters when defining the function
+// billAmount and taxRate are parameters
+function calculateBillTwo(billAmount, taxRate) {
+    console.log("running Calculate Bill Two function");
+    const total = billAmount * (1 + taxRate);  
+    return total;
+}
+// 100, 0.13 are called as arguments
+const myTotalTwo = calculateBillTwo(100, 0.13);
+console.log(`total bill is $${myTotal}`);
+
+//passing variables/references into functions
+const newTotal = 500;
+const newTaxRate = 0.2;
+console.log(`total bill for the newTotal newTaxRate is $${calculateBillTwo(newTotal, newTaxRate)}`)
