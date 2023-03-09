d92502d832f2319e1fe3dc96c8c3f072911c4532
Jiang <jiangyue0380@gmail.com>
2023-03-09 12:07:10 +1100

    updated examples of difference between event.target and event.currentTarget, stop event in capture down or bubbling phase

--- Module-5-Events/event-listener.js
+++ Module-5-Events/event-listener.js
@@ -28,14 +28,39 @@ myButton.addEventListener('mouseout', (event) => {
 // you have to use a named function in order to use removeEventListener
 //myButton.removeEventListener('click', handleClick)
 
-//listen on multiple items
-const myButtons = document.querySelectorAll('.buy');
-function handleBuyBtnClick(event) {
-    console.log(`you have bought this item ${event.target.textContent}`)
-}
-function attachBuyBtnListener(btn) {
-    btn.addEventListener('click', (e) => handleBuyBtnClick(e));
-}
-myButtons.forEach(attachBuyBtnListener)
 
-// console.log(myButtons)
+const myButtons = document.querySelectorAll('.buy');
+/**listen on multiple items */
+// function handleBuyBtnClick(event) {
+//     console.log(`you have bought this item ${event.target.textContent}`)
+// }
+// function attachBuyBtnListener(btn) {
+//     btn.addEventListener('click', (e) => handleBuyBtnClick(e));
+// }
+// myButtons.forEach(attachBuyBtnListener);
+
+/** understand the event param */
+function handleBuyBtnClick(event) {
+    // console.log('you are buying this item');
+    // console.log(event);
+    console.log(event.target);//elements that actually got clicked
+    console.log(event.currentTarget);//element that fires up the eventListner
+    //console.log(parseFloat(event.target.dataset.price))
+    
+    // stop this event from bubbling up
+    // event.stopPropagation();
+}
+
+myButtons.forEach(function(buyBtn) {
+    buyBtn.addEventListener('click', handleBuyBtnClick);
+})
+
+
+window.addEventListener('click', function(event) {
+    console.log('You clicked the window');
+    //stop this event from capture down, the handleBuyBtnClick will never fire up
+    event.stopPropagation();
+}, {
+    capture: true,
+})
+
