fe6de00546f641c2af672b3085e74aa2713775e1
Jiang <jiangyue0380@gmail.com>
2023-03-10 00:20:06 +1100

    finalised the click 'Learn more' button to open the modal with the information of the card it gets clicked on

---
+++ Module-6-Practice/Click-Outside/click-outside.js
@@ -0,0 +1,23 @@
+const cardBtns = document.querySelectorAll(".card button");
+const modalOuter = document.querySelector('.modal-outer');
+const modalInner = document.querySelector('.modal-inner')
+const modalCloseBtn = document.querySelector('.close-modal');
+
+function handleCardBtnClick(event) {
+    const button = event.currentTarget;
+    const card = button.closest('.card')
+    const cardImg = card.querySelector('img').cloneNode(true);
+    const cardTitle = card.querySelector('h2').cloneNode(true);
+    modalOuter.classList.add('open');
+    modalInner.insertAdjacentElement('beforeend', cardImg)
+    cardImg.insertAdjacentElement('afterend', cardTitle)
+}
+
+function handleModalCloseClick() {
+    modalOuter.classList.remove('open');
+}
+cardBtns.forEach((btn) => {
+    btn.addEventListener('click', handleCardBtnClick)
+})
+
+modalCloseBtn.addEventListener('click', handleModalCloseClick)
