369cc5c3e6c0cc9849db2b3b95d377a4bba60245
Jiang <jiangyue0380@gmail.com>
2023-03-16 23:57:36 +1100

    updated the solution of click buttons go through the tabs

--- Module-6-Practice/Tabs/tab.js
+++ Module-6-Practice/Tabs/tab.js
@@ -4,7 +4,25 @@ const tabPanels = tabs.querySelectorAll('[role="tabpanel"]');
 
 
 function handleTabBtnClick(e) {
-    console.log(e.target)
+    
+    tabPanels.forEach(panel => {
+        // hide all tab panels
+        panel.hidden = true;
+    })
+    
+    
+    tabBtns.forEach(tab => {
+        // mark all tabs as unselected
+        tab.setAttribute('aria-selected', false);
+    })
+    // mark this tab as selected
+    e.target.setAttribute('aria-selected', true);
+    // find the assocaited tabPanel and show it
+    const id = e.target.id;
+    const tabPanel = tabs.querySelector(`[aria-labelledby="${id}"]`);
+    tabPanel.hidden = false;
+
+
 }
 
 tabBtns.forEach(button => button.addEventListener('click', handleTabBtnClick))
