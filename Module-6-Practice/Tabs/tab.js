dc2cc1de43722792b9dc405000cd3684dbc4efb0
Jiang <jiangyue0380@gmail.com>
2023-03-17 00:08:15 +1100

    updated the solution by using find method

--- Module-6-Practice/Tabs/tab.js
+++ Module-6-Practice/Tabs/tab.js
@@ -1,16 +1,11 @@
 const tabs = document.querySelector('.tabs');
 const tabBtns = tabs.querySelectorAll('[role="tab"]');
-const tabPanels = tabs.querySelectorAll('[role="tabpanel"]');
-
-
+const tabPanels = Array.from(tabs.querySelectorAll('[role="tabpanel"]'));
 function handleTabBtnClick(e) {
-    
     tabPanels.forEach(panel => {
         // hide all tab panels
         panel.hidden = true;
     })
-    
-    
     tabBtns.forEach(tab => {
         // mark all tabs as unselected
         tab.setAttribute('aria-selected', false);
@@ -19,8 +14,17 @@ function handleTabBtnClick(e) {
     e.target.setAttribute('aria-selected', true);
     // find the assocaited tabPanel and show it
     const id = e.target.id;
-    const tabPanel = tabs.querySelector(`[aria-labelledby="${id}"]`);
-    tabPanel.hidden = false;
+    // const tabPanel = tabs.querySelector(`[aria-labelledby="${id}"]`);
+    // tabPanel.hidden = false;
+    // use the find method
+    const tabPanel = tabPanels.find(panel => {
+      if(panel.getAttribute('aria-labelledby') === id) {
+        panel.hidden = false;
+      } else {
+        panel.hidden = true;
+      }
+    })
+    console.log(tabPanel)
 
 
 }
