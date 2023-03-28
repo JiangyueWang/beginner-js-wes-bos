ae633bae65a481f7b86300843662209f0287a01e
Jiang <jiangyue0380@gmail.com>
2023-03-28 23:49:49 +1100

    updated instance methods examples of slice,indexOf, includes, reverse

--- Module-8-Data-types/arrays/array-cardio.js
+++ Module-8-Data-types/arrays/array-cardio.js
@@ -103,8 +103,35 @@ console.log(firstItem);
 toppings.unshift(firstItem);
 // Do the last four,but immutable (with spreads and new variables)
 let newToppings = toppings.slice(0, toppings.length - 1);
-console.log(toppings);//
+console.log(toppings);//['Mushrooms ', 'Tomatoes', 'Eggs', 'Chili', 'Lettuce', 'Avocado', 'Chiles', 'Bacon', 'Pickles', 'Onions', 'Cheese']
 console.log(newToppings);//['Mushrooms ', 'Tomatoes', 'Eggs', 'Chili', 'Lettuce', 'Avocado', 'Chiles', 'Bacon', 'Pickles', 'Onions']
-newtoppings = [...newToppings, toppings[toppings.length - 1]];
-console.log(newtoppings);
+newtoppings = [...newToppings, toppings[toppings.length - 1]];//put the last item from the original toppings into newToppings
+console.log(newtoppings);//['Mushrooms ', 'Tomatoes', 'Eggs', 'Chili', 'Lettuce', 'Avocado', 'Chiles', 'Bacon', 'Pickles', 'Onions', 'Cheese']
 
+
+// Make a copy of the toppings array with slice()
+const toppingsCopy = toppings.slice(0);
+toppings[0] = 'Mushy Boi';
+console.log(toppings);
+console.log(toppingsCopy);
+// Make a copy of the toppings array with a spread
+const toppingsCopy2 = [...toppings];
+// take out items 3 to 5 of your new toppings array with splice()
+toppingsCopy.splice(3, 3);
+console.log(toppingsCopy);
+// find the index of Avocado with indexOf() / lastIndexOf()
+const avoIndex = toppings.indexOf('Avocado');
+console.log(avoIndex);
+
+// Check if hot sauce is in the toppings with includes()
+const isInToppings = toppings.includes('Hot Sauce');
+console.log(isInToppings);
+// add it if it's not
+if (!isInToppings) {
+  toppings.push('Hot Sauce');
+}
+console.log(toppings);
+// flip those toppings around with reverse()
+// toppings.reverse();
+const toppingsReversed = [...toppings].reverse();
+console.log(toppingsReversed);
