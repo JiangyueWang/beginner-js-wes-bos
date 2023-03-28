f907a0daa4fd5eb9a590181683f4447bcc690aae
Jiang <jiangyue0380@gmail.com>
2023-03-28 23:46:09 +1100

    updated instance method examples for array

--- Module-8-Data-types/arrays/array-cardio.js
+++ Module-8-Data-types/arrays/array-cardio.js
@@ -50,16 +50,61 @@ function createRange(start, end) {
 };
 
 // take the meats object and make these arrays with Object.entries(); Object.keys(), Object.values()
-console.log(Object.entries(meats));//return array
-console.log(Object.keys(meats));//return arrays
-console.log(Object.values(meats));//return arrays
+// console.log(Object.entries(meats));//return array
+// console.log(Object.keys(meats));//return arrays
+// console.log(Object.values(meats));//return arrays
 
-Object.values(meats).forEach(qty => {
-    console.log(qty)
-})
+// Object.values(meats).forEach(qty => {
+//     // console.log(qty)
+// })
 
-Object.values(meats).forEach(qty => {
-    const key = entry[0];
-    const value = entity[1];
-    console.log(key, value);
-})
+// Object.values(meats).forEach(qty => {
+//     const key = entry[0];
+//     const value = entity[1];
+//     // console.log(key, value);
+// })
+
+
+
+/** instance method */
+
+// .join()
+// array1.join() turns array1 into a string with comma
+// console.log(["egg", "wonder", "brioche"].join());//egg,wonder,brioche
+// display all bun types with 'or' -  use join()
+//console.log(buns.join(' or '));//egg or wonder or brioche
+
+//.split()
+// string1.split() turns string1 into an array
+// turn string "hot dogs, hamburgers, sausages, corn" into an array
+const foodString = "hot dogs, hamburgers, sausages, corn";
+//console.log(foodString.split(','));//['hot dogs', ' hamburgers', ' sausages', ' corn']
+//console.log(foodString.split('')); //['h', 'o', 't', ' ', 'd', 'o', 'g', 's', ',', ' ', 'h', 'a', 'm', 'b', 'u', 'r', 'g', 'e', 'r', 's', ',', ' ', 's', 'a', 'u', 's', 'a', 'g', 'e', 's', ',', ' ', 'c', 'o', 'r', 'n']
+
+//.pop(), a mutable method!
+//array1.pop() returns the last item within the array, and remove it from the array1
+// console.log(toppings);//['Mushrooms ', 'Tomatoes', 'Eggs', 'Chili', 'Lettuce', 'Avocado', 'Chiles', 'Bacon', 'Pickles', 'Onions', 'Cheese']
+// const lastItem = toppings.pop();
+// console.log(lastItem);//Cheese
+// console.log(toppings);/['Mushrooms ', 'Tomatoes', 'Eggs', 'Chili', 'Lettuce', 'Avocado', 'Chiles', 'Bacon', 'Pickles', 'Onions']
+
+//.push()
+// array1.push() add an item to the end of an array
+// console.log(toppings);//['Mushrooms ', 'Tomatoes', 'Eggs', 'Chili', 'Lettuce', 'Avocado', 'Chiles', 'Bacon', 'Pickles', 'Onions', 'Cheese']
+// toppings.push('pizza');
+// console.log(toppings);//['Mushrooms ', 'Tomatoes', 'Eggs', 'Chili', 'Lettuce', 'Avocado', 'Chiles', 'Bacon', 'Pickles', 'Onions', 'Cheese', 'pizza']
+
+//.shift()
+//.unshift()
+// take the first item off toppings with shift()
+const firstItem = toppings.shift();
+console.log(firstItem);
+// add it back in with unshift()
+toppings.unshift(firstItem);
+// Do the last four,but immutable (with spreads and new variables)
+let newToppings = toppings.slice(0, toppings.length - 1);
+console.log(toppings);//
+console.log(newToppings);//['Mushrooms ', 'Tomatoes', 'Eggs', 'Chili', 'Lettuce', 'Avocado', 'Chiles', 'Bacon', 'Pickles', 'Onions']
+newtoppings = [...newToppings, toppings[toppings.length - 1]];
+console.log(newtoppings);
+
