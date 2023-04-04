99db48fb687007ceb14e91124aa9f9baa3434cc5
testuser <jiangyue0380@gmail.com>
2023-04-05 00:54:00 +1000

    updated gallery.js with open modal with clicked img information

--- Module-10-Practice/Gallery-Practice/gallery.js
+++ Module-10-Practice/Gallery-Practice/gallery.js
@@ -1,1 +1,50 @@
-console.log('it works')
+// select modal elements
+const modal = document.querySelector('.modal');
+const prevBtn = modal.querySelector('.prev');
+const nextBtn = modal.querySelector('.next');
+const modalImg = modal.querySelector('img');
+
+
+function Gallery(gallery) {
+    if(!gallery) {
+        throw new Error('No Gallery Found!');
+    }
+
+    function openModal() {
+        console.info('Opening Modal...');
+        // First check if the modal is already open
+        if (modal.matches('.open')) {
+          console.info('Madal already open');
+          return; // stop the function from running
+        }
+        modal.classList.add('open');
+      }
+    function showImg(el) {
+        if(!el) {
+            console.info('no image to show');
+            return;
+        }
+
+        // updated modal with cliked image information
+        console.log(el);
+        
+        modalImg.src = el.src;
+        modal.querySelector('h2').textContent = el.title;
+        modal.querySelector('figure p').textContent = el.dataset.description;
+
+        openModal();
+
+    }
+
+    // select images from gallery 
+    const images = Array.from(gallery.querySelectorAll('img'));
+    images.forEach(img => {
+        img.addEventListener('click', (e) => showImg(e.target));
+    })
+
+    
+}
+
+// select galleries from the page ensure the reusability of the Gallery function
+const gallery1 = Gallery(document.querySelector('.gallery1'));
+const gallery2 = Gallery(document.querySelector('.gallery2'));
